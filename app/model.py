f33e30b3b7ce241aedfb075621898540098096ed
inergon3 <mr.morala@mail.ru>
2025-04-04 20:08:20 +0700

    update models2

--- app/model.py
+++ app/model.py
@@ -1,10 +1,13 @@
 import datetime
 from importlib.metadata import metadata
 
-from sqlalchemy import DateTime, ForeignKey
+from sqlalchemy import DateTime, ForeignKey, MetaData
+from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker
 from sqlalchemy.orm import as_declarative, Mapped, mapped_column
 
-
+metadata = MetaData()
+engine = create_async_engine(f"postgresql+asyncpg://{user}:{password_bd}@{host}/{db_name}", echo=True)
+SessionLocal = async_sessionmaker(autocommit=False, autoflush=False, bind=engine)
 @as_declarative(metadata=metadata)
 class AbstractModel:
     pass
@@ -16,6 +19,8 @@ class SecretModel(AbstractModel):
     secret: Mapped[str] = mapped_column()
     passphrase: Mapped[str] = mapped_column(unique=True)
     secret_key: Mapped[str] = mapped_column(unique=True)
+    get_secret: Mapped[bool] = mapped_column()
+    del_secret: Mapped[bool] = mapped_column()
 
 
 class AddSecretModel(AbstractModel):
